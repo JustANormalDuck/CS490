bf09877ef85cc743ccc920acc620e096145b711b
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-09-21 11:02:20 -0400

    Update back_login.php
    added json configuration

--- back/back_login.php
+++ back/back_login.php
@@ -1,33 +1,37 @@
 <?php
 require_once("db.php");
-
 //get username password from jerry
-$username = null;
-$password = null;
+$obj = new stdClass();
+$obj->username = $_POST['username'];
+$obj->password = $_POST['password'];
 
 $db = getDB();
 if (isset($db)){
     $stmt = $db->prepare("SELECT username, hash, role from Users WHERE username = :username");
-    $params = array(":username" => $username);
+    $params = array(":username" => $obj->username);
     $r = $stmt->execute($params);
-    echo "DB RETURNED: " . var_export($r, true);
+    $obj->error = "DB RETURNED: " . var_export($r, true);
     $e = $stmt->errorInfo();
     if ($e[0] != "00000"){
-        echo "Something went wrong with " . $stmt;
+        $obj->error = "Something went wrong with " . $stmt;
     }
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($result && isset($result["hash"]) && isset($result["role"])){
-        if (password_verify($password, $result["hash"])){
+        if (password_verify($obj->password, $result["hash"])){
             if($result["role"] == "teacher"){
-                echo "REDIRECT TO TEACHER PAGE";
+                $obj->role = "T"; //T for teacher
             } else {
-                echo "REDIRECT TO STUDENT PAGE";
+                $obj->role = "S"; //S for student
             }
         } else {
-            echo "There was a validation issue";
+            $obj->role = "V"; //V for validation issue
         }
     } else {
-        echo "Invalid user";
+        $obj->role = "I"; //I for invalid user
     }
+} else {
+    $obj->error = "Error setting db";
 }
+
+echo json_encode($obj);
 ?>
