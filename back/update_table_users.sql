62f360da538b92b9330391440edbc7ab038b8c88
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-09-22 10:53:41 -0400

    added users theo and jerry, teacher and student

---
+++ back/update_table_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO `jmf64`.`Users` (`username`, `hash`, `role`) VALUES ('theo', '$2y$10$JxxchiWZDIP5QbtZql6U6.46C6GgFCAllV4fnY96HrmwsnTzLgiZC', 'teacher');
+INSERT INTO `jmf64`.`Users` (`username`, `hash`, `role`) VALUES ('jerry', '$2y$10$SqmnuIztQ1k9Oj.tksyUreCjRWzFrXjP1wpO/j51I2Qr1Jtrrh32e', 'student');
