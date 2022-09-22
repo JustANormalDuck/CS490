6872847df2b65ec59e5ff258f68b48cfd3c920f4
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-09-21 23:05:09 -0400

    Update create_table_users.sql

--- back/create_table_users.sql
+++ back/create_table_users.sql
@@ -1,1 +1,1 @@
-CREATE TABLE IF NOT EXISTS `Users` (`username` VARCHAR(50) NOT NULL, `hash` VARCHAR(50) NOT NULL, `role` VARCHAR(7) NOT NULL, PRIMARY KEY (`username`), UNIQUE (`username`))
+CREATE TABLE IF NOT EXISTS `Users` (`username` VARCHAR(50) NOT NULL, `hash` VARCHAR(100) NOT NULL, `role` VARCHAR(7) NOT NULL, PRIMARY KEY (`username`), UNIQUE (`username`))
