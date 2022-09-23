fb70bb92508c576f5d63d4853e23178d1af5a3c0
Jerry Zheng <91507126+jerryz18@users.noreply.github.com>
2022-09-23 08:46:27 -0400

    Basic Login Page using curl

---
+++ front/login.php
@@ -0,0 +1,47 @@
+<?php
+  if (isset($_POST['submit'])) 
+  {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $URL= 'https://afsaccess4.njit.edu/~nk82/middle_login.php';
+    $post_params="username=$username&password=$password";
+    $ch = curl_init();
+    $options = array(CURLOPT_URL => $URL,
+               CURLOPT_HTTPHEADER =>
+         array('Content-type:application/x-www-form-urlencoded'),
+         CURLOPT_RETURNTRANSFER => TRUE,
+         CURLOPT_POST => TRUE,
+         CURLOPT_POSTFIELDS => $post_params);
+    curl_setopt_array($ch, $options);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $decodedData = json_decode($result, true);
+    if ($decodedData["role"] == "T"){
+      die(header('Location: teacher.php'));
+    }
+    else if ($decodedData["role"] == "S"){
+      die(header('Location: student.php'));
+    }
+    else if ($decodedData["role"] == "I" || $decodedData["role"] == "V"){
+      echo "Invalid password or username";
+    }
+    else{
+      echo "Unknown Error";
+    }
+    echo "<form action=\"\" method=\"post\">";
+    echo "Username: <input name=\"username\" type=\"text\" />";
+    echo "Password: <input name=\"password\" type=\"text\" />";
+    echo "<input name=\"submit\" type=\"submit\" />";
+    echo "</form>";
+  }
+  
+  else {
+        
+    echo "<form action=\"\" method=\"post\">";
+    echo "Username: <input name=\"username\" type=\"text\" />";
+    echo "Password: <input name=\"password\" type=\"password\" />";
+    echo "<input name=\"submit\" type=\"submit\" />";
+    echo "</form>";
+
+  }
+?>
