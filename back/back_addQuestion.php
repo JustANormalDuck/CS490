1049caf091446cfc12f38c119ef15c9c27c229ff
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-10-04 11:00:29 -0400

    add ability to insert questions into db

---
+++ back/back_addQuestion.php
@@ -0,0 +1,25 @@
+<?php
+require_once("db.php");
+//get difficulty topic questoiin from jerry
+$obj = new stdClass();
+$obj->difficulty = $_POST['difficulty'];
+$obj->topic = $_POST['topic'];
+$obj->question = $_POST['question'];
+
+$db = getDB();
+if (isset($db)) {
+    $stmt = $db->prepare("INSERT INTO Questions(difficulty, topic, question) VALUES(:difficulty, :topic, :question)");
+    $params = array(":difficulty" => $obj->difficulty, ":topic" => $obj->topic, ":question" => $obj->question);
+    $r = $stmt->execute($params);
+    $e = $stmt->errorInfo();
+    if ($e[0] == "00000") {
+        $obj->error = "Successfully entered question";
+    } else {
+        $obj->error = "An error occurred when inserting the question";
+    }
+} else {
+    $obj->error = "Error setting db";
+}
+
+echo json_encode($obj);
+?>
