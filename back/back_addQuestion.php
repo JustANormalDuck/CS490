50966709b37afcc83d3553f46fdaaee7ba21da1d
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-10-06 20:54:38 -0400

    added ability to add test cases to questions table

--- back/back_addQuestion.php
+++ back/back_addQuestion.php
@@ -5,11 +5,29 @@ $obj = new stdClass();
 $obj->difficulty = $_POST['difficulty'];
 $obj->topic = $_POST['topic'];
 $obj->question = $_POST['question'];
+$obj->funcName = $_POST['function'];
+$obj->case1 = $_POST['case1'];
+$obj->case2 = $_POST['case2'];
+if (!isset($obj->$_POST['case3'])){
+    $obj->case3 = "ITANI";
+} else {
+    $obj->case3 = $_POST['case3'];
+}
+if (!isset($obj->$_POST['case4'])){
+    $obj->case4 = "ITANI";
+} else {
+    $obj->case4 = $_POST['case4'];
+}
+if (!isset($obj->$_POST['case5'])){
+    $obj->case5 = "ITANI";
+} else {
+    $obj->case5 = $_POST['case5'];
+}
 
 $db = getDB();
 if (isset($db)) {
-    $stmt = $db->prepare("INSERT INTO Questions(difficulty, topic, question) VALUES(:difficulty, :topic, :question);");
-    $params = array(":difficulty" => $obj->difficulty, ":topic" => $obj->topic, ":question" => $obj->question);
+    $stmt = $db->prepare("INSERT INTO Questions(difficulty, topic, question, func_name, case1, case2, case3, case4, case5) VALUES(:difficulty, :topic, :question, :func_name, :case1, :case2, :case3, :case4, :case5);");
+    $params = array(":difficulty" => $obj->difficulty, ":topic" => $obj->topic, ":question" => $obj->question, ":func_name" => $obj->funcName, ":case1" => $obj->case1, ":case2" => $obj->case2, ":case3" => $obj->case3, ":case4" => $obj->case4, ":case5" => $obj->case5);
     $r = $stmt->execute($params);
     $e = $stmt->errorInfo();
     if ($e[0] == "00000") {
