ceb7cb5e625fd7adab6608fc15cad97233fc570a
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-10-22 17:31:44 -0400

    add student responses to grades table

--- back/back_addDefaultGrade.php
+++ back/back_addDefaultGrade.php
@@ -4,6 +4,7 @@ require_once("db.php");
 $obj = new stdClass();
 $obj->username = $_POST['username'];
 $obj->examID = $_POST['exam_id'];
+$obj->responses = $_POST['student_responses'];
 $obj->earnedPoints = "ITANI"; //also default for updated points
 $obj->possiblePoints = "ITANI";
 $obj->grade = -1;
@@ -12,8 +13,8 @@ $obj->released = 0;
 
 $db = getDB();
 if (isset($db)){
-    $stmt = $db->prepare("INSERT INTO Grades(username, exam_id, earned_points, updated_points, possible_points, grade, comments, released) VALUES(:username, :exam_id, :earned, :updated, :possible, :grade, :comment, :released);");
-    $params = array(":username" => $obj->username, ":exam_id" => $obj->examID, ":earned" => $obj->earnedPoints, ":updated" => $obj->earnedPoints, ":possible" => $obj->possiblePoints, ":grade" => $obj->comment, ":released" => $obj->released);
+    $stmt = $db->prepare("INSERT INTO Grades(username, exam_id, student_responses, earned_points, updated_points, possible_points, grade, comments, released) VALUES(:username, :exam_id, :responses, :earned, :updated, :possible, :grade, :comment, :released);");
+    $params = array(":username" => $obj->username, ":exam_id" => $obj->examID, ":responses" => $obj->responses, ":earned" => $obj->earnedPoints, ":updated" => $obj->earnedPoints, ":possible" => $obj->possiblePoints, ":grade" => $obj->comment, ":released" => $obj->released);
     $r = $stmt->execute($params);
     $e = $stmt->errorInfo();
     if ($e[0] == "00000") {
