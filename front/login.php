9d5c674a9cc81244b3f56df10304324da8aa0248
Jerry Zheng <91507126+jerryz18@users.noreply.github.com>
2022-10-22 23:53:28 -0400

    updated ui

--- front/login.php
+++ front/login.php
@@ -1,6 +1,7 @@
 <?php
   if (isset($_POST['submit'])) 
   {
+    ob_start();
     $username = $_POST['username'];
     $password = $_POST['password'];
     $URL= 'https://afsaccess4.njit.edu/~nk82/middle_login.php';
@@ -18,30 +19,46 @@
     $decodedData = json_decode($result, true);
     if ($decodedData["role"] == "T"){
       die(header('Location: teacher.php'));
+      exit();
     }
     else if ($decodedData["role"] == "S"){
       die(header('Location: student.php'));
+      exit();
     }
     else if ($decodedData["role"] == "I" || $decodedData["role"] == "V"){
-      echo "Invalid password or username";
+      echo '<p class="sign" align="center">Invalid Username or Password</p>';
     }
     else{
       echo "Unknown Error";
     }
-    echo "<form action=\"\" method=\"post\">";
-    echo "Username: <input name=\"username\" type=\"text\" />";
-    echo "Password: <input name=\"password\" type=\"password\" />";
-    echo "<input name=\"submit\" type=\"submit\" />";
+    echo '<div class="main">';
+    echo '<p class="sign" align="center">Sign in</p>';
+    echo "<form class='form1' action=\"\" method=\"post\">";
+    echo "<input class='un ' type='text' align='center' name=\"username\" type=\"text\" placeholder='Username'/>";
+    echo "<input class='pass' type='password' align='center' name=\"password\" type=\"password\" placeholder='Password'/>";
+    echo "<input class='submit' name=\"submit\" type=\"submit\" align='center'/>";
     echo "</form>";
+    echo "</div>";
   }
   
   else {
-        
-    echo "<form action=\"\" method=\"post\">";
-    echo "Username: <input name=\"username\" type=\"text\" />";
-    echo "Password: <input name=\"password\" type=\"password\" />";
-    echo "<input name=\"submit\" type=\"submit\" />";
+    
+    echo '<div class="main">';
+    echo '<p class="sign" align="center">Sign in</p>';
+    echo "<form class='form1' action=\"\" method=\"post\">";
+    echo "<input class='un ' type='text' align='center' name=\"username\" type=\"text\" placeholder='Username'/>";
+    echo "<input class='pass' type='password' align='center' name=\"password\" type=\"password\" placeholder='Password'/>";
+    echo "<input class='submit' name=\"submit\" type=\"submit\" align='center'/>";
     echo "</form>";
+    echo "</div>";
 
   }
 ?>
+
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="logincss.css">
+  <title>Login</title>
+</head>
