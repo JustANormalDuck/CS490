b233e9fa28cd970b0cb195c9ed690ce29a8ca7e9
JustANormalDuck <65052456+JustANormalDuck@users.noreply.github.com>
2022-10-26 00:38:14 -0400

    Add files via upload

--- middle/middle_addExam.php
+++ middle/middle_addExam.php
@@ -4,7 +4,7 @@ $questionNumList = $_POST['question_num_list'];
 $questionPointList = $_POST['question_point_list'];
 $questionIdList = $_POST['question_id_list'];
 $URL= 'https://afsaccess4.njit.edu/~jmf64/back_addDefaultGrade.php';
-$post_params="test_name=$testName&question_point_list=$questionNumList&question_point_list=$questionPointList&question_id_list=$questionIdList";
+$post_params="test_name=$testName&question_num_list=$questionNumList&question_point_list=$questionPointList&question_id_list=$questionIdList";
 $ch = curl_init();
 $options = array(CURLOPT_URL => $URL,
 			         CURLOPT_HTTPHEADER =>
