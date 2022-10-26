fe6c9fe154a368753e60c0f56dd980809029e36f
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-10-26 14:00:12 -0400

    Update back_pullExams.php

--- back/back_pullExams.php
+++ back/back_pullExams.php
@@ -7,7 +7,7 @@ $obj->username = $_POST["username"];
 $db = getDB();
 if (isset($db)){
     //need to change to greater than 3 that way it doesnt pull the dummy exams
-    $stmt = $db->prepare("SELECT id, test_name, question_num_list, question_id_list from Exams WHERE id > 0 and id not in (Select exam_id from Grades where username = :username);");
+    $stmt = $db->prepare("SELECT * from Exams WHERE id > 0 and id not in (Select exam_id from Grades where username = :username);");
     $params = array(":username" => $obj->username);
     $r = $stmt->execute($params);
     $e = $stmt->errorInfo();
@@ -19,16 +19,19 @@ if (isset($db)){
         $ids = array();
         $names = array();
         $questions = array();
+        $points = array();
         $questionIDs = array();
         foreach ($exams as $ee){
             array_push($ids, $ee["id"]);
             array_push($names, $ee["test_name"]);
             array_push($questions, $ee["question_num_list"][strlen($ee["question_num_list"])-1]);
+            array_push($points, $ee["question_point_list"]);
             array_push($questionIDs, $ee["question_id_list"]);
         }
         $obj->id = $ids;
         $obj->testName = $names;
         $obj->numQuestions = $questions;
+        $obj->pointList = $points;
         $obj->qIDs = $questionIDs;
         $obj->error = "Exams successfully returned";
     } else {
