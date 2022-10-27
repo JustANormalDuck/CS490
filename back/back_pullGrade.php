934ced36eb223f595f0f28eb8cc4d21db21dcdbb
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-10-27 01:23:05 -0400

    Add files via upload

---
+++ back/back_pullGrade.php
@@ -0,0 +1,39 @@
+<?php
+require_once("db.php");
+//pull grade from Grades table to send to jerry for updating
+$obj = new stdClass();
+$obj->username = $_POST["username"];
+$obj->examID = $_POST["exam_id"];
+
+$db = getDB();
+if (isset($db)){
+    $stmt = $db->prepare("SELECT username, exam_id, test_name, student_responses, earned_points, updated_points, possible_points, grade, comments from Grades WHERE username = :username and exam_id = :exam_id;");
+    $params = array(":username" => $obj->username, ":exam_id" => $obj->examID);
+    $r = $stmt->execute($params);
+    $e = $stmt->errorInfo();
+    if ($e[0] != "00000"){
+        $obj->debug = "Something went wrong with error code " . $e[0];
+    }
+    $grade = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (count($grade) == 1){
+        foreach ($grade as $g){
+            $obj->username = $g["username"];
+            $obj->examID = $g["exam_id"];
+            $obj->testName = $g["test_name"];
+            $obj->studentResponses = $g["student_responses"];
+            $obj->earnedPoints = $g["earned_points"];
+            $obj->updatedPoints = $g["updated_points"];
+            $obj->possiblePoints = $g["possible_points"];
+            $obj->grade = $g["grade"];
+            $obj->comments = $g["comments"];
+            $obj->error = "Grade successfully returned";
+        }
+    } else {
+        $obj->error = "No grade returned";
+    }
+} else {
+    $obj->error = "Error setting db";
+}
+
+echo json_encode($obj);
+?>
---
+++ back/back_pullUngraded.php
@@ -0,0 +1,54 @@
+<?php
+require_once("db.php");
+//pull grades from Grades table to send to jerry for autograding
+$obj = new stdClass();
+
+$db = getDB();
+if (isset($db)){
+    $stmt = $db->prepare("SELECT username, exam_id, test_name, student_responses, earned_points, updated_points, possible_points, grade, comments from Grades WHERE grade = -1;");
+    $r = $stmt->execute();
+    $e = $stmt->errorInfo();
+    if ($e[0] != "00000"){
+        $obj->debug = "Something went wrong with error code " . $e[0];
+    }
+    $grades = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (count($grades) > 0){
+        $usernames = array();
+        $ids = array();
+        $names = array();
+        $responses = array();
+        $earned = array();
+        $updated = array();
+        $possible = array();
+        $scores = array();
+        $teacherComments = array();
+        foreach ($grades as $g){
+            array_push($usernames, $g["username"]);
+            array_push($ids, $g["exam_id"]);
+            array_push($names, $g["test_name"]);
+            array_push($responses, $g["student_responses"]);
+            array_push($earned, $g["earned_points"]);
+            array_push($updated, $g["updated_points"]);
+            array_push($possible, $g["possible_points"]);
+            array_push($scores, $g["grade"]);
+            array_push($teacherComments, $g["comments"]);
+        }
+        $obj->username = $usernames;
+        $obj->examID = $ids;
+        $obj->testName = $names;
+        $obj->studentResponses = $responses;
+        $obj->earnedPoints = $earned;
+        $obj->updatedPoints = $updated;
+        $obj->possiblePoints = $possible;
+        $obj->grade = $scores;
+        $obj->comments = $teacherComments;
+        $obj->error = "Grades successfully returned";
+    } else {
+        $obj->error = "No grades returned";
+    }
+} else {
+    $obj->error = "Error setting db";
+}
+
+echo json_encode($obj);
+?>
