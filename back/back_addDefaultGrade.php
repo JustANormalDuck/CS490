f4da8a6400afb4393dce7a5c6266b44798bc4318
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-11-01 19:45:01 -0400

    add auto comments

--- back/back_addDefaultGrade.php
+++ back/back_addDefaultGrade.php
@@ -9,13 +9,13 @@ $obj->responses = $_POST['student_responses'];
 $obj->earnedPoints = "ITANI"; //also default for updated points
 $obj->possiblePoints = $_POST["possible_points"];
 $obj->grade = -1;
-$obj->comment = "ITANI";
+$obj->comment = "ITANI"; //also default for auto comments
 $obj->released = 0;
 
 $db = getDB();
 if (isset($db)){
-    $stmt = $db->prepare("INSERT INTO Grades(username, exam_id, test_name, student_responses, earned_points, updated_points, possible_points, grade, comments, released) VALUES(:username, :exam_id, :test_name, :responses, :earned, :updated, :possible, :grade, :comment, :released);");
-    $params = array(":username" => $obj->username, ":exam_id" => $obj->examID, ":test_name" => $obj->testName, ":responses" => $obj->responses, ":earned" => $obj->earnedPoints, ":updated" => $obj->earnedPoints, ":possible" => $obj->possiblePoints, ":grade" => $obj->grade, ":comment" => $obj->comment, ":released" => $obj->released);
+    $stmt = $db->prepare("INSERT INTO Grades(username, exam_id, test_name, student_responses, earned_points, updated_points, possible_points, grade, auto_comments, comments, released) VALUES(:username, :exam_id, :test_name, :responses, :earned, :updated, :possible, :grade, :auto, :comment, :released);");
+    $params = array(":username" => $obj->username, ":exam_id" => $obj->examID, ":test_name" => $obj->testName, ":responses" => $obj->responses, ":earned" => $obj->earnedPoints, ":updated" => $obj->earnedPoints, ":possible" => $obj->possiblePoints, ":grade" => $obj->grade, ":auto" => $obj->comment, ":comment" => $obj->comment, ":released" => $obj->released);
     $r = $stmt->execute($params);
     $e = $stmt->errorInfo();
     if ($e[0] == "00000") {
