ab9a8be1139b57608e8789f8905112949ac0fd77
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-11-01 13:49:14 -0400

    add constraint

--- back/back_pullTestCases.php
+++ back/back_pullTestCases.php
@@ -1,12 +1,12 @@
 <?php
 require_once("db.php");
-//pull function name and all test cases from Questions table to send to nalby for autograde comparisons
+//pull constraint function name and all test cases from Questions table to send to nalby for autograde comparisons
 $obj = new stdClass();
 $obj->questionID = $_POST['question_id'];
 
 $db = getDB();
 if (isset($db)){
-    $stmt = $db->prepare("SELECT func_name, case1, case2, case3, case4, case5 from Questions WHERE id = :question_id;");
+    $stmt = $db->prepare("SELECT konstraint, func_name, case1, case2, case3, case4, case5 from Questions WHERE id = :question_id;");
     $params = array(":question_id" => $obj->questionID);
     $r = $stmt->execute($params);
     $e = $stmt->errorInfo();
@@ -16,6 +16,7 @@ if (isset($db)){
     $cases = $stmt->fetchAll(PDO::FETCH_ASSOC);
     if (count($cases) == 1){
         foreach ($cases as $c){
+            $obj->constraint = $c["konstraint"];
             $obj->func_name = $c["func_name"];
             $obj->case1 = $c["case1"];
             $obj->case2 = $c["case2"];
