a7c22f23f0e6d12bb62ed0b8be4c5982878acd24
Jerry Zheng <91507126+jerryz18@users.noreply.github.com>
2022-11-03 20:12:04 -0400

    added constraint and more test cases

--- front/addquestion.php
+++ front/addquestion.php
@@ -5,11 +5,38 @@
     $topic = $_POST['qTopic'];
     $question = $_POST['question'];
     $funcName = $_POST['functionName'];
-    $case1 = $_POST['testCase1'];
-    $case2 = $_POST['testCase2'];
-
+    $input1 = $_POST['input1'];
+    $input2 = $_POST['input2'];
+    $output1 = $_POST['output1'];
+    $output2 = $_POST['output2'];
+    //pass to backend the form functionName(input)?output
+    $case1 = $funcName. "(" . $input1 . ")?" . $output1;
+    $case2 = $funcName. "(" . $input2 . ")?" . $output2; 
     $URL= 'https://afsaccess4.njit.edu/~nk82/middle_addQuestion.php';
     $post_params="difficulty=$difficulty&topic=$topic&question=$question&function=$funcName&case1=$case1&case2=$case2";
+
+    if (isset($_POST['input3']))
+    {
+      $input3 = $_POST['input3'];
+      $output3 = $_POST['output3'];
+      $case3 = $funcName. "(" . $input3 . ")?" . $output3;
+      $post_params .= "&case3=$case3";
+    }
+
+    if (isset($_POST['input4']))
+    {
+      $input4 = $_POST['input4'];
+      $output4 = $_POST['output4'];
+      $post_params .= "&case4=$case4";
+    }
+
+    if (isset($_POST['input5']))
+    {
+      $input5 = $_POST['input5'];
+      $output5 = $_POST['output5'];
+      $post_params .= "&case5=$case5";
+    }
+
     $ch = curl_init();
     $options = array(CURLOPT_URL => $URL,
                    CURLOPT_HTTPHEADER =>
@@ -52,9 +79,29 @@
     echo "<input name=\"functionName\" type=\"text\" placeholder='Function Name'/>";
     echo "</br></br>";
 
-    echo "<label> Test Cases must be passed in the form functionName(input)?output</label>  </br> ";
-    echo "<input name=\"testCase1\" type=\"text\" placeholder='Test Case 1'/>";
-    echo "<input name=\"testCase2\" type=\"text\" placeholder='Test Case 2'/>";
+    echo "<label> Choose a Constraint: </label>";
+    echo "<select name='konstraint' id='konstraint'>";
+    echo "<option value='None' selected> None </option>";
+    echo "<option value='For'> For </option>";
+    echo "<option value='While'> While </option>";
+    echo "<option value='Recursion'> Recursion </option>";
+    echo "</select>";
+    echo "</br></br>";
+
+    echo "<label> Enter Input Test Cases </label>  </br> ";
+    echo "<input name=\"input1\" type=\"text\" placeholder='Input Case 1' required/>";
+    echo "<input name=\"input2\" type=\"text\" placeholder='Input Case 2' required/>";
+    echo "<input name=\"input3\" type=\"text\" placeholder='Input Case 3' />";
+    echo "<input name=\"input4\" type=\"text\" placeholder='Input Case 4' />";
+    echo "<input name=\"input5\" type=\"text\" placeholder='Input Case 5' />";
+    echo "</br></br>";
+
+    echo "<label> Enter Output Test Cases </label>  </br> ";
+    echo "<input name=\"output1\" type=\"text\" placeholder='Output Case 1' required/>";
+    echo "<input name=\"output2\" type=\"text\" placeholder='Output Case 2' required/>";
+    echo "<input name=\"output3\" type=\"text\" placeholder='Output Case 3' />";
+    echo "<input name=\"output4\" type=\"text\" placeholder='Output Case 4' />";
+    echo "<input name=\"output5\" type=\"text\" placeholder='Output Case 5' />";
     echo "</br></br>";
 
     echo "<input class='submit' name=\"submit\" type=\"submit\" align='center'/>";
