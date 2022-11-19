1ba914bd7062ad1cc313186978078f17890896ac
jmf64 <70586786+jmf64@users.noreply.github.com>
2022-11-19 13:56:12 -0500

    add auto comments

--- back/back_updateGrades.php
+++ back/back_updateGrades.php
@@ -6,6 +6,7 @@ $obj->username = $_POST["username"];
 $obj->examID = $_POST["exam_id"];
 $obj->updatedPoints = $_POST["updated_points"];
 $obj->grade = $_POST["grade"];
+$obj->auto = $_POST["auto_comments"];
 if (!isset($_POST["comments"])){
     $obj->comments = "No teacher comments";
 } else {
@@ -14,8 +15,8 @@ if (!isset($_POST["comments"])){
 
 $db = getDB();
 if (isset($db)){
-    $stmt = $db->prepare("UPDATE Grades SET updated_points = :updatedPoints, grade = :grade, comments = :comments, released = 1 WHERE username = :username and exam_id = :examID;");
-    $params = array(":updatedPoints" => $obj->updatedPoints, ":grade" => $obj->grade, ":comments" => $obj->comments, ":username" => $obj->username, ":examID" => $obj->examID);
+    $stmt = $db->prepare("UPDATE Grades SET updated_points = :updatedPoints, grade = :grade, auto_comments = :auto, comments = :comments, released = 1 WHERE username = :username and exam_id = :examID;");
+    $params = array(":updatedPoints" => $obj->updatedPoints, ":grade" => $obj->grade, ":auto" => $obj->auto, ":comments" => $obj->comments, ":username" => $obj->username, ":examID" => $obj->examID);
     $r = $stmt->execute($params);
     $e = $stmt->errorInfo();
     if ($e[0] == "00000") {
